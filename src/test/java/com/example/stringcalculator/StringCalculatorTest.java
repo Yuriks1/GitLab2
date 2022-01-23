9f344ab8125ebf518bdde7b26e3f8b00a84b62ee
Yuriks1 <89460670+Yuriks1@users.noreply.github.com>
2022-01-23 14:53:07 +0100

    Add test for one number

--- src/test/java/com/example/stringcalculator/StringCalculatorTest.java
+++ src/test/java/com/example/stringcalculator/StringCalculatorTest.java
@@ -1,17 +1,24 @@
 package com.example.stringcalculator;
-
 import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class StringCalculatorTest {
+  class StringCalculatorTest {
+
+    StringCalculator stringCalculator = new StringCalculator();
 
     @Test
-    void addEmptyStingShouldReturnZero() {
+    void addEmptyStringShouldReturnZero() {
         StringCalculator stringCalculator = new StringCalculator();
 
         int result = stringCalculator.add("");
 
         assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    void addOneNumberShouldReturnOne() {
+        int result = stringCalculator.add("1");
+
+        assertThat(result).isEqualTo(1);
     }
 }
