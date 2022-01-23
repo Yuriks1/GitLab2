2260f9ccfd692b922275e14caae78cbc3af0b6ee
Yuriks1 <89460670+Yuriks1@users.noreply.github.com>
2022-01-23 15:02:47 +0100

    Add test for adding two numbers

--- src/main/java/com/example/stringcalculator/StringCalculator.java
+++ src/main/java/com/example/stringcalculator/StringCalculator.java
@@ -2,7 +2,14 @@ package com.example.stringcalculator;
 
 public class StringCalculator {
 
-    public int add(String s) {
-        return 0;
+    public int add(String numbers) {
+
+        if (numbers.equals("")) {
+            return 0;
+        } else if (numbers.contains(",")) {
+            return Integer.parseInt(numbers.substring(0, 1)) + Integer.parseInt(numbers.substring(2, 3));
+        } else {
+            return Integer.parseInt(numbers);
+        }
     }
 }
--- src/test/java/com/example/stringcalculator/StringCalculatorTest.java
+++ src/test/java/com/example/stringcalculator/StringCalculatorTest.java
@@ -2,23 +2,29 @@ package com.example.stringcalculator;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
-  class StringCalculatorTest {
+class StringCalculatorTest {
 
     StringCalculator stringCalculator = new StringCalculator();
 
     @Test
     void addEmptyStringShouldReturnZero() {
-        StringCalculator stringCalculator = new StringCalculator();
 
         int result = stringCalculator.add("");
-
         assertThat(result).isEqualTo(0);
     }
-
     @Test
     void addOneNumberShouldReturnOne() {
+
         int result = stringCalculator.add("1");
 
         assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    void addTwoNumbersShouldReturnThree() {
+
+        int result = stringCalculator.add("1,2");
+
+        assertThat(result).isEqualTo(3);
     }
 }
